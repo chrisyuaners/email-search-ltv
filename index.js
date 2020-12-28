cabcc4191d36056b1a3571b7b07622d3044d040f
chrisyuaners <chrisyuaners@gmail.com>
2020-12-28 18:02:17 -0500

    add initial landing page html tags and data

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// declare variables
+const URL = 'https://ltv-data-api.herokuapp.com/api/v1/records.json?email=';
+
+// wait for file to load before running JS code
 $(document).ready(function() {
-    console.log('HELLO');
+    console.log('JS LOADING...');
+    // add event listeners
+    $( "#search-form" ).submit(function( event ) {
+        event.preventDefault();
+        console.log($("#email").val());
+        fetch(`${URL}${$("#email").val()}`, {mode: 'no-cors'})
+        .then(resp => resp.json())
+        .then(result => {
+            console.log(result);
+        })
+    });
+    console.log('JS LOADED');
 });
