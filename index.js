b7d45204f1c035bf9aa17c9990f44005a91b6222
chrisyuaners <chrisyuaners@gmail.com>
2020-12-28 19:12:43 -0500

    add loading page and styles

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 // declare variables
 const URL = 'https://ltv-data-api.herokuapp.com/api/v1/records.json?email=';
+const mainContainer = document.querySelector('main');
+const initialLanding = $('#landing-initial');
 
 // wait for file to load before running JS code
 $(document).ready(function() {
     console.log('JS LOADING...');
+    // code templates
+    const loading = `
+        <div id="loading">
+            <img src="assets/loading_spinner.gif" alt="spinner" />
+            <div id="loading-text">Please wait a moment...</div>
+        </div>
+    `;
+
     // add event listeners
     $( "#search-form" ).submit(function( event ) {
         event.preventDefault();
-        console.log($("#email").val());
-        fetch(`${URL}${$("#email").val()}`, {mode: 'no-cors'})
-        .then(resp => resp.json())
-        .then(result => {
-            console.log(result);
-        })
+        const userData = {};
+        const input = $("#email").val();
+        $("#email").val('');
+        initialLanding.hide();
+        mainContainer.innerHTML = loading;
+        // fetch(`${URL}${$("#email").val()}`)
+        // .then(resp => resp.json())
+        // .then(result => {
+        //     console.log(result);
+        // })
     });
     console.log('JS LOADED');
 });
