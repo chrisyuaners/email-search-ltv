a3419c079d97b7996ef0394ce732d3995105efb0
chrisyuaners <chrisyuaners@gmail.com>
2021-01-04 13:04:16 -0500

    CORS bypass

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 A mobile responsive email query application that returns any matching results.
 
-Made with jQuery, vanilla JS, HTML and CSS.
+Made with some jQuery, mostly vanilla JS, HTML and CSS.
 
 Hosted on: http://important-branch.surge.sh
--- index.js
+++ index.js
@@ -158,85 +158,33 @@ $(document).ready(function() {
         initialLanding.innerHTML = '';
         initialLanding.innerHTML = loading;
         // fetch request with email input from user
-        // fetch(`${URL}${$("#email").val()}`)
-        // .then(resp => resp.json())
-        // .then(result => {
-        //     console.log(result);
-        // })
-        setTimeout(function() {
-            if (input === 'jonsmith@example.com') {
-                const user = {
-                    "email": "jonsmith@example.com",
-                    "first_name": "Jon",
-                    "last_name": "Smith",
-                    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                    "address": "123 Chocolate Ave",
-                    "phone_numbers": [
-                    "2125551234",
-                    "2125551235",
-                    "2125551236"
-                    ],
-                    "relatives": [
-                    "Jane Smith",
-                    "Doe Smith"
-                    ]
-                };
-                userData.push(user);
-            } else if (input === 'janesmith@example.com') {
-                const user = {
-                    "email": "janesmith@example.com",
-                    "first_name": "Jane",
-                    "last_name": "Smith",
-                    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                    "address": "123 Chocolate Ave",
-                    "phone_numbers": [
-                    "2125551234",
-                    "2125551235",
-                    "2125551236"
-                    ],
-                    "relatives": [
-                    "Jon Smith",
-                    "Doe Smith"
-                    ]
-                };
-                userData.push(user);
-            } else if (input === 'doesmith@example.com') {
-                const user = {
-                    "email": "doesmith@example.com",
-                    "first_name": "Dow",
-                    "last_name": "Smith",
-                    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                    "address": "123 Chocolate Ave",
-                    "phone_numbers": [
-                    "2125551234",
-                    "2125551235",
-                    "2125551236"
-                    ],
-                    "relatives": [
-                    "Jane Smith",
-                    "Jon Smith"
-                    ]
-                };
-                userData.push(user);
-            }
-            
-            if (userData.length === 0) {
-                initialLanding.innerHTML = noResults;
-                searchAgain.addClass("search-again-show");
-            } else {
-                let resultCount = 0;
-                initialLanding.innerHTML = results;
-                userData.forEach(user => {
-                    const userResult = renderResult(user);
-                    const resultsList = document.querySelector('#results');
-                    resultsList.innerHTML += userResult;
-                    resultCount++;
-                })
-                const resultTitle = document.querySelector("#results-title");
-                resultTitle.innerHTML = resultCount > 1 ? `${resultCount} Results` : '1 Result';
-                searchAgain.addClass("search-again-show");
-            }
-        }, 2000);
+        fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(`${URL}${input}`)}`)
+            .then(response => {
+                if (response.ok) return response.json()
+                throw new Error('Network response was not ok.')
+            })
+            .then(data => {
+                const parsedData = JSON.parse(data.contents);
+                if (parsedData.length === undefined) {
+                    userData.push(parsedData);
+                }
+                if (userData.length === 0) {
+                    initialLanding.innerHTML = noResults;
+                    searchAgain.addClass("search-again-show");
+                } else {
+                    let resultCount = 0;
+                    initialLanding.innerHTML = results;
+                    userData.forEach(user => {
+                        const userResult = renderResult(user);
+                        const resultsList = document.querySelector('#results');
+                        resultsList.innerHTML += userResult;
+                        resultCount++;
+                    })
+                    const resultTitle = document.querySelector("#results-title");
+                    resultTitle.innerHTML = resultCount > 1 ? `${resultCount} Results` : '1 Result';
+                    searchAgain.addClass("search-again-show");
+                }
+            });
     });
 
     emailInput.keyup(function() {
